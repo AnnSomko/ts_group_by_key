d3c482ffc63a89d27afd543be021d3c4bd570a1b
Anna <ania.somko@gmail.com>
2025-01-17 21:41:02 +0200

    bugs fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,10 +2,7 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T extends GroupsMap<T>>(
-  items: T[],
-  key: keyof T,
-): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   return items.reduce((obj: GroupsMap<T>, item: T) => {
     const groupKey = String(item[key]);
 
